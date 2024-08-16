ec127317459069d093a2eca88d967ded5c6bf043
tharun-01 <57319240+tharun-01@users.noreply.github.com>
2024-08-16 13:56:01 +0530

    init

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HomeController {
+
+    @PostMapping(value = "/")
+    public ResponseEntity health(){
+        log.info("request received");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
